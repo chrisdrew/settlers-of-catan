346f90ca722b41de80c64d5c083ce28921af92b1
Keaton Bishop-Marx <keaton22@gmail.com>
2019-03-31 15:05:55 -0400

    mass js restructuring

--- scripts.js
+++ scripts.js
@@ -1,41 +1,73 @@
-var model = [];
+function initialize() {
 
-var tokens = [];
+    var boardModel = [];
+    this.myPlayer = {};
+    this.conn = null;
 
-var gridDOM = document.querySelector('#hex-layer');        
-var columns = gridDOM.children;
+    initializeBoard(boardModel);
+    initializeMyPlayer(myPlayer);
+    initializeMyPeer(myPlayer);
 
-var rowCount = 0;
+} 
 
-for (var i = 0; i < columns.length; i++) {
+function initializeBoard(boardModel) {
 
-    model.push([]);                                     // create a new array for each column
+    mapBoard(boardModel);
 
-    var rows = columns[i].children;
+    var forestsCount = countHexesByType('forest', boardModel);
+    var hillsCount = countHexesByType('hill', boardModel);
+    var pasturesCount = countHexesByType('pasture', boardModel);
+    var fieldsCount = countHexesByType('field', boardModel);
+    var mountainsCount = countHexesByType('mountain', boardModel);
+    var desertsCount = countHexesByType('desert', boardModel);
+    var seasCount = countHexesByType('sea', boardModel);
+    var spacersCount = countHexesByType('spacer', boardModel);
 
-    rowCount = 0;
+    var tokensCount = forestsCount + hillsCount + pasturesCount + fieldsCount + mountainsCount;     // count how many tokens we need
 
-    for (var ii = 0; ii < rows.length; ii++) {
+    var tokens = generateTokens(tokensCount);           // generate the appropriate number of tokens
+    assignTokensToHexes(tokens, boardModel);            // randomly assign tokens to hexes
 
-        var row = rows[ii];
+    console.log(boardModel);
+}
 
-        var type = row.getAttribute('data-type');
+function mapBoard(boardModel) {       // create an object model of the game board
 
-        model[i].push({});
+    var boardDOM = document.querySelector('#hex-layer');        
+    var columns = boardDOM.children;
 
-        if (type !== 'desert' && type !== 'sea' && type !== 'spacer') {
+    var rowCount = 0;
 
-            rowCount++;
+    for (var i = 0; i < columns.length; i++) {
 
-            var id = (i + 1) + '-' + (rowCount + 1);            // adding 1 to i and ii lets us start counting at col 1 and row 1
-            row.id = id;
+        boardModel.push([]);                                     // create a new array for each column
 
-            model[i][ii].id = id;
+        var rows = columns[i].children;
+
+        rowCount = 0;
+
+        for (var ii = 0; ii < rows.length; ii++) {
+
+            var row = rows[ii];
+
+            var type = row.getAttribute('data-type');
+
+            boardModel[i].push({});
+
+            if (type !== 'desert' && type !== 'sea' && type !== 'spacer') {
+
+                rowCount++;
+
+                var id = (i + 1) + '-' + (rowCount + 1);            // adding 1 to i and ii lets us start counting at col 1 and row 1
+                row.id = id;
+
+                boardModel[i][ii].id = id;
+            }
+
+            boardModel[i][ii].type = type;
         }
 
-        model[i][ii].type = type;
     }
-
 }
 
 function random(min, max) {
@@ -47,11 +79,11 @@ function convertNodeListToArray(nodelist) {
     return arr;
 }
 
-function countHexesByType(type) {
+function countHexesByType(type, boardModel) {
     var count = 0;
 
-    for (var i = 0; i < model.length; i++) {
-        var column = model[i];
+    for (var i = 0; i < boardModel.length; i++) {
+        var column = boardModel[i];
         for (var ii = 0; ii < column.length; ii++) {
             var row = column[ii];
             if (row.type === type) {
@@ -84,18 +116,18 @@ function generateTokens(count) {            // remember that we need (number of 
     return tokens;
 }
 
-function assignTokensToHexes(tokens) {
+function assignTokensToHexes(tokens, boardModel) {
     var rowCount = 0;
     
-    for (var i = 0; i < model.length; i++) {
-        var column = model[i];
+    for (var i = 0; i < boardModel.length; i++) {
+        var column = boardModel[i];
         rowCount = 0;
         for (var ii = 0; ii < column.length; ii++) {
             var row = column[ii];
             if (row.type !== 'desert' && row.type !== 'sea' && row.type !== 'spacer') {
                 rowCount++;
                 var token = tokens.pop();                   // set the token equal to the last value in the array
-                row.token = token;                          // assign the token in the model 
+                row.token = token;                          // assign the token in the boardModel 
                 //var id = (i + 1) + '-' + (rowCount);        // build the ID so we can select the appropriate element
 
                 //if (!!document.getElementById(id)) {        // if this coordinate contains a valid hex type
@@ -106,20 +138,60 @@ function assignTokensToHexes(tokens) {
     }
 }
 
-//console.log(generateTokens(30));
+function initializeMyPlayer(myPlayer) {
+    var name = prompt('Enter your name:');
+    myPlayer.name = name;
+}
 
-var forestsCount = countHexesByType('forest');
-var hillsCount = countHexesByType('hill');
-var pasturesCount = countHexesByType('pasture');
-var fieldsCount = countHexesByType('field');
-var mountainsCount = countHexesByType('mountain');
-var desertsCount = countHexesByType('desert');
-var seasCount = countHexesByType('sea');
-var spacersCount = countHexesByType('spacer');
+function initializeMyPeer(myPlayer) {
+    this.myPeer = new Peer(myPlayer.name, {
+        key: 'lwjd5qra8257b9',
+        config: {'iceServers': [
+            {
+                url: 'stun:stun.l.google.com:19302'
+            },
+            {
+                url: 'turn:turn.bistri.com:80',
+                username: 'homeo',
+                credential: 'homeo'
+            }
+        ]}
+    });
 
-var tokensCount = forestsCount + hillsCount + pasturesCount + fieldsCount + mountainsCount;
+    this.myPeer.on('open', function () {
+        console.info('my peer has been initialized');
+        initializeConnection();
+    });
+}
 
-var tokens = generateTokens(tokensCount);
-assignTokensToHexes(tokens);
+function initializeConnection() {
+    this.myPeer.on('connection', function (conn) {
+        conn.on('data', function (data) {
+            alert(data);
+        });
+        console.log('peer connection established')
+    });
+}
 
-console.log(model);
+document.querySelector('#connect').onclick = function () {
+    connectToPeer(this.myPlayer);
+}.bind(this);
+
+function connectToPeer(myPlayer) {
+    var otherPlayer = prompt('Join a player:');
+    this.conn = this.myPeer.connect(otherPlayer);
+
+    this.conn.on('open', function () {
+        this.conn.send(this.myPlayer.name + ' joined your game!');
+    }.bind(this));
+}
+
+document.querySelector('#ping').onclick = function () {
+    ping(this.conn);
+}.bind(this);
+
+function ping(conn) {
+    if (conn) {
+        conn.send('pinged by ' + this.myPlayer.name);
+    }
+}
